b5e60f3192f78ca3ab486332a76d892332af1afa
utkarshgarg62 <utkarshgarg143@gmail.com>
2022-06-04 22:22:05 +0530

    players-appi

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,42 @@
 const express = require('express');
-
 const router = express.Router();
+ 
 
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
-});
-
+   router.post('/players', function (req, res) {
+    let players =
+    [
+        {
+            "name": "manish",
+            "dob": "1/1/1995",
+            "gender": "male",
+            "city": "jalandhar",
+            "sports": [
+                "swimming"
+            ]
+        },
+        {
+            "name": "gopal",
+            "dob": "1/09/1995",
+            "gender": "male",
+            "city": "delhi",
+            "sports": [
+                "soccer"
+            ],
+        },
+        {
+            "name": "lokesh",
+            "dob": "1/1/1990",
+            "gender": "male",
+            "city": "mumbai",
+            "sports": [
+                "soccer"
+            ],
+        },
+    ]
+  
+    let ele=req.body;
+    players.push(ele)
+       res.send(  { data: players , status: true }  )
+   })
+  
 module.exports = router;
-// adding this comment for no reason
