d8b302102edb55dcbe30cb7107ed193502b44b9a
utkarshgarg62 <utkarshgarg143@gmail.com>
2022-06-05 02:15:14 +0530

    get-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,41 +2,80 @@ const express = require('express');
 const router = express.Router();
  
 
-   router.post('/players', function (req, res) {
-    let players =
-    [
-        {
-            "name": "manish",
-            "dob": "1/1/1995",
-            "gender": "male",
-            "city": "jalandhar",
-            "sports": [
-                "swimming"
-            ]
-        },
-        {
-            "name": "gopal",
-            "dob": "1/09/1995",
-            "gender": "male",
-            "city": "delhi",
-            "sports": [
-                "soccer"
-            ],
-        },
-        {
-            "name": "lokesh",
-            "dob": "1/1/1990",
-            "gender": "male",
-            "city": "mumbai",
-            "sports": [
-                "soccer"
-            ],
-        },
-    ]
-  
-    let ele=req.body;
-    players.push(ele)
-       res.send(  { data: players , status: true }  )
+   router.get('/movies', function (req, res) {
+    let movieList =
+    ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+
+       res.send(movieList);
    })
+
+
+   router.get('/movies/:indexNumber', function (req, res) {
+    let movieList =
+    ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    for (i=0;i=movieList.length;i++){
+        let i=req.params.indexNumber;
+        if(i>movieList.length){
+            res.send('No movie at this index number')
+        }
+        else{
+            res.send(movieList[i]);
+
+        }
+    }
+   })
+
+   
+   router.get('/films', function (req, res) {
+    let filmList =
+    [ {
+        id: 1,
+        name: "The Shining"
+       }, {
+        id: 2,
+        name: "Incendies"
+       }, {
+        id: 3,
+        name: "Rang de Basanti"
+       }, {
+        id: 4,
+        name: "Finding Nemo"
+       }]
+       res.send(filmList);
+    })
+       
+
+
+
+   router.get('/films/:filmId', function (req, res) {
+    let filmList =
+    [ {
+        id: 1,
+        name: "The Shining"
+       }, {
+        id: 2,
+        name: "Incendies"
+       }, {
+        id: 3,
+        name: "Rang de Basanti"
+       }, {
+        id: 4,
+        name: "Finding Nemo"
+       }]
+       
+            for (i=0;i=filmList.length;i++){
+                let i=req.params.filmId-1;
+                if(i>filmList.length){
+                    res.send('No movie exists with this id');
+                }
+                
+                else{
+                    res.send(filmList[i]);
+                }
+            }
+       
+   })
+
+
   
 module.exports = router;
