cfdf96b02f271a731456bcad88d47bf436dbc00f
utkarshgarg62 <utkarshgarg143@gmail.com>
2022-06-07 03:53:40 +0530

    book-schema

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,20 +1,29 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
+    bookName:{
+        type: String ,
+        required: true
+    } ,
     prices: {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+    year:{
+        type: Number,
+        default: 2021
+    },
+    tags: [String],
+    authorName: String, 
+    totalPages: Number, 
+    
+    stockAvailable: Boolean,
+    
+    //sales: {type: Number, default: 10}
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+module.exports = mongoose.model('newBooks', bookSchema) //books
 
 //Validation:
 //require:true
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+const bookController= require("../controllers/bookController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createBook", bookController.createBook  )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/bookList", bookController.bookList)
 
-router.post("/createBook", BookController.createBook  )
+router.get("/getBookInYear", bookController.getBookInYear  )
 
-router.get("/getBooksData", BookController.getBooksData)
+router.get("/getParticularBooks/:key", bookController.getParticularBooks)
+
+router.get("/getXINRBooks", bookController.getXINRBooks)
+
+router.get("/getRandomBooks", bookController.getRandomBooks)
+
 
 module.exports = router;
