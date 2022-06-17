4eb6614a84d58bc5cc9700530a1cc7a46dad205a
utkarshgarg62 <utkarshgarg143@gmail.com>
2022-06-17 16:22:23 +0530

    newass

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -79,7 +79,29 @@ let getOtp = async function (req, res) {
 }
 
 
+let getByDistrict = async function (req, res) {
+    try {
+        let id = req.query.district_id
+        let date = req.query.date
+        console.log(`query params are: ${id} ${date}`)
+        var options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${id}&date=${date}`
+        }
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+
+
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.getByDistrict = getByDistrict
